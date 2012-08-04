aadd1e76c692d9d9f28d79e2a4949e16e82492fb
sgnn7 <sgnn7@sgnn7.org>
2012-08-04 20:01:05 +0100

    Replaced slider with seek bar -sgnn7

--- Android2Linux/src/org/sgnn7/android2linux/widgets/FilesizeChooserView.java
+++ Android2Linux/src/org/sgnn7/android2linux/widgets/FilesizeChooserView.java
@@ -1,17 +1,10 @@
 package org.sgnn7.android2linux.widgets;
 
-import org.sgnn7.android2linux.R;
-
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
-public class FilesizeChooserView extends RelativeLayout {
-	private ImageView sliderBackground;
-	private ImageView sliderButton;
-
+public class FilesizeChooserView extends SeekBar {
 	public FilesizeChooserView(Context context) {
 		super(context);
 
@@ -25,20 +18,7 @@ public class FilesizeChooserView extends RelativeLayout {
 	}
 
 	private void initialize(Context context) {
-		sliderBackground = new ImageView(context);
-		sliderBackground.setImageDrawable(context.getResources().getDrawable(R.drawable.slider_bg));
-		sliderBackground.setScaleType(ScaleType.CENTER_INSIDE);
-		sliderBackground.setAdjustViewBounds(true);
-
-		sliderButton = new ImageView(context);
-		sliderButton.setImageDrawable(context.getResources().getDrawable(R.drawable.slider_button));
-		sliderBackground.setScaleType(ScaleType.CENTER_INSIDE);
-		sliderButton.setAdjustViewBounds(true);
-
-		LayoutParams layoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		layoutParams.setMargins(0, 6, 0, 0);
-		addView(sliderBackground, layoutParams);
-		addView(sliderButton, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-
+		setMax(4096);
+		setProgress(2048);
 	}
 }
